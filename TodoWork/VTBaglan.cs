13852bd691a2ecab72d2f7ffcc93f65a6923205d
Onur Yüksekkaya <sosyal@onuryuksekkaya.com>
2021-01-31 17:45:38 +0300

    master

---
+++ TodoWork/VTBaglan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TodoWork
+{
+    class VTBaglan
+    {
+        public SqlConnection bagla()
+        {
+            SqlConnection bagla = new SqlConnection(@"Data Source=DESKTOP-JIG192K;Initial Catalog=isTakip;Integrated Security=True");
+            bagla.Open();
+            return bagla;
+        }
+    }
+}
